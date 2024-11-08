3996d749c25bb46e746ac67fea76c596934ab65b
Matteo Tortora <matteo.tortora@gmail.com>
2024-11-08 23:07:08 +0100

    upd

--- src/info/Info.js
+++ src/info/Info.js
@@ -24,7 +24,7 @@ of what each of the values mean.
 export const info = {
     firstName: "Matteo",
     lastName: "Tortora",
-    initials: "mt", // the example uses first and last, but feel free to use three or more if you like.
+    initials: "MT", // the example uses first and last, but feel free to use three or more if you like.
     position: "an Assistant Professor in AI",
     selfPortrait: self, // don't change this unless you want to name your self-portrait in the "img" folder something else!
     gradient: `-webkit-linear-gradient(135deg, ${colors})`, // don't change this either
